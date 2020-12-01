3f0ff0056caf6d4571b58536c4465714d0d1d426
cmj <cmj@cmj.tw>
2020-12-01 09:38:00 +0800

    [Setter] fix linter

--- callback.go
+++ callback.go
@@ -13,7 +13,7 @@ var (
 type Callback func(parser *ArgParse) bool
 
 func RegisterCallback(name string, fn Callback) {
-	if _, ok := callback[name]; ok {
+	if _, ok := callbacks[name]; ok {
 		// show the alert
 		Log(WARN, "duplicated callback %v, override", name)
 	}
